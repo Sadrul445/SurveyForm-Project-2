90f7c515697413868e092f061dd2bdb134e645df
Sadrul Tanim Chowdhury <42459631+Sadrul445@users.noreply.github.com>
2021-02-17 11:38:56 +0600

    Add files via upload

---
+++ indexx.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+   <?php 
+    
+    $title= " Your Form Fill-Up is Completed !!! ";
+    echo $title;
+    ?>
+</body>
+</html>
